6a350ed70d6c9ce1a773d69d8525a0a5e1d486e3
Thomas Moffet <pantherman1996@gmail.com>
2021-12-01 16:20:08 +1100

    Extracted common test input to const.

--- Tests/Day01Tests.cs
+++ Tests/Day01Tests.cs
@@ -7,8 +7,10 @@ using Xunit;
 namespace Tests;
 
 public class Day01Tests {
+	private const string TestInput = "199,200,208,210,200,207,240,269,260,263";
+
 	[Theory]
-	[InlineData("199,200,208,210,200,207,240,269,260,263", 7)]
+	[InlineData(TestInput, 7)]
 	public void PartOneSucceeds(string values, int expectedResult) {
 		Day01 challenge = new();
 		List<int> parsedInput = values.Split(',').Select(x => int.Parse(x)).ToList();
@@ -16,7 +18,7 @@ public class Day01Tests {
 	}
 
 	[Theory]
-	[InlineData("199,200,208,210,200,207,240,269,260,263", 5)]
+	[InlineData(TestInput, 5)]
 	public void PartTwoSucceeds(string values, int expectedResult) {
 		Day01 challenge = new();
 		List<int> parsedInput = values.Split(',').Select(x => int.Parse(x)).ToList();
