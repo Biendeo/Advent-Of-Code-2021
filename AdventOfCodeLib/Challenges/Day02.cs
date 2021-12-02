8de0b2e6a35a37e7e2a8a38b74eefb812a3bcb59
Thomas Moffet <pantherman1996@gmail.com>
2021-12-02 16:16:44 +1100

    Solution for day 2. Wonder if there's a better way to present it.

---
+++ AdventOfCodeLib/Challenges/Day02.cs
@@ -0,0 +1,74 @@
+﻿namespace AdventOfCodeLib.Challenges;
+
+[DayDetails(Day = 2, Name = "Tomorrow")]
+public class Day02 : IDayChallenge {
+	public string PartOneFromFile(string[] inputLines) => PartOne(inputLines).ToString();
+
+	public int PartOne(string[] inputLines) {
+		List<Step> input = ParseInput(inputLines);
+		int x = 0, y = 0;
+
+		foreach (Step step in input) {
+			switch (step.Direction) {
+				case Direction.Up:
+					y -= step.Magnitude;
+					break;
+				case Direction.Forward:
+					x += step.Magnitude;
+					break;
+				case Direction.Down:
+					y += step.Magnitude;
+					break;
+			}
+		}
+
+		return Math.Abs(x * y);
+	}
+
+	public string PartTwoFromFile(string[] inputLines) => PartTwo(inputLines).ToString();
+
+	public int PartTwo(string[] inputLines) {
+		List<Step> input = ParseInput(inputLines);
+		int x = 0, y = 0, aim = 0;
+
+		foreach (Step step in input) {
+			switch (step.Direction) {
+				case Direction.Up:
+					aim -= step.Magnitude;
+					break;
+				case Direction.Forward:
+					x += step.Magnitude;
+					y += step.Magnitude * aim;
+					break;
+				case Direction.Down:
+					aim += step.Magnitude;
+					break;
+			}
+		}
+
+		return Math.Abs(x * y);
+	}
+
+	private List<Step> ParseInput(string[] inputLines) => inputLines.Select(l => new Step(l.Split(" ")[0], int.Parse(l.Split(" ")[1]))).ToList();
+
+	private enum Direction {
+		Forward,
+		Up,
+		Down
+	}
+
+	private class Step {
+		public Direction Direction;
+		public int Magnitude;
+
+		public Step(string direction, int magnitude) {
+			Direction = direction switch {
+				"forward" => Direction.Forward,
+				"up" => Direction.Up,
+				"down" => Direction.Down,
+				_ => throw new Exception("Bad input!")
+			};
+			Magnitude = magnitude;
+		}
+	}
+}
---
+++ Tests/Day02Tests.cs
@@ -0,0 +1,25 @@
+﻿using AdventOfCodeLib.Challenges;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Tests;
+
+public class Day02Tests {
+	private const string TestInput = "forward 5,down 5,forward 8,up 3,down 8,forward 2";
+
+	[Theory]
+	[InlineData(TestInput, 150)]
+	public void PartOneSucceeds(string values, int expectedResult) {
+		Day02 challenge = new();
+		Assert.Equal(expectedResult, challenge.PartOne(values.Split(',')));
+	}
+
+	[Theory]
+	[InlineData(TestInput, 900)]
+	public void PartTwoSucceeds(string values, int expectedResult) {
+		Day02 challenge = new();
+		Assert.Equal(expectedResult, challenge.PartTwo(values.Split(',')));
+	}
+}
