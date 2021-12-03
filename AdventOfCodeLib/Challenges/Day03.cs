3fb57f166559641741883254cf45ce63bd0b57b8
Thomas Moffet <pantherman1996@gmail.com>
2021-12-03 16:45:02 +1100

    Completed day 3. It works but it can definitely be better.

---
+++ AdventOfCodeLib/Challenges/Day03.cs
@@ -0,0 +1,31 @@
+﻿namespace AdventOfCodeLib.Challenges;
+
+[DayDetails(Day = 3, Name = "Binary Diagnostic")]
+public class Day03 : IDayChallenge {
+	public string PartOneFromFile(string[] inputLines) => PartOne(inputLines).ToString();
+
+	public int PartOne(string[] input) {
+		int gammaRate = Convert.ToInt32(string.Concat(Enumerable.Range(0, input.First().Length).Select(index => IsMostCommonDigitInPositionOne(input, index) ? '1' : '0')), 2);
+		int epsilonRate = (1 << input.First().Length) - 1 - gammaRate;
+		return gammaRate * epsilonRate;
+	}
+
+	public string PartTwoFromFile(string[] inputLines) => PartTwo(inputLines).ToString();
+
+	public int PartTwo(string[] input) {
+		List<string> remainingOxygenNumbers = new(input), remainingCo2ScrubberNumbers = new(input);
+		for (int i = 0; i < input.First().Length; ++i) {
+			if (remainingOxygenNumbers.Count > 1) {
+				char charToKeep = IsMostCommonDigitInPositionOne(remainingOxygenNumbers, i) ? '1' : '0';
+				remainingOxygenNumbers.RemoveAll(s => s[i] != charToKeep);
+			}
+			if (remainingCo2ScrubberNumbers.Count > 1) {
+				char charToRemove = IsMostCommonDigitInPositionOne(remainingCo2ScrubberNumbers, i) ? '1' : '0';
+				remainingCo2ScrubberNumbers.RemoveAll(s => s[i] == charToRemove);
+			}
+		}
+		return Convert.ToInt32(string.Concat(remainingOxygenNumbers.Single()), 2) * Convert.ToInt32(string.Concat(remainingCo2ScrubberNumbers.Single()), 2);
+	}
+
+	private bool IsMostCommonDigitInPositionOne(IEnumerable<string> values, int index) => values.Count(v => v[index] == '1') * 2 >= values.Count();
+}
---
+++ Tests/Day03Tests.cs
@@ -0,0 +1,22 @@
+﻿using AdventOfCodeLib.Challenges;
+using Xunit;
+
+namespace Tests;
+
+public class Day03Tests {
+	private const string TestInput = "00100,11110,10110,10111,10101,01111,00111,11100,10000,11001,00010,01010";
+
+	[Theory]
+	[InlineData(TestInput, 198)]
+	public void PartOneSucceeds(string values, int expectedResult) {
+		Day03 challenge = new();
+		Assert.Equal(expectedResult, challenge.PartOne(values.Split(',')));
+	}
+
+	[Theory]
+	[InlineData(TestInput, 230)]
+	public void PartTwoSucceeds(string values, int expectedResult) {
+		Day03 challenge = new();
+		Assert.Equal(expectedResult, challenge.PartTwo(values.Split(',')));
+	}
+}
