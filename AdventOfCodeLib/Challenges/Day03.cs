f2f139d84a221c44655294d06e3a9f766ce47cec
Thomas Moffet <pantherman1996@gmail.com>
2021-12-03 17:00:25 +1100

    Reworked mostly part 2, but it's a lot shorter and easier to take in.

--- AdventOfCodeLib/Challenges/Day03.cs
+++ AdventOfCodeLib/Challenges/Day03.cs
@@ -5,27 +5,24 @@ public class Day03 : IDayChallenge {
 	public string PartOneFromFile(string[] inputLines) => PartOne(inputLines).ToString();
 
 	public int PartOne(string[] input) {
-		int gammaRate = Convert.ToInt32(string.Concat(Enumerable.Range(0, input.First().Length).Select(index => IsMostCommonDigitInPositionOne(input, index) ? '1' : '0')), 2);
+		int gammaRate = BinaryToInt(Enumerable.Range(0, input.First().Length).Select(index => IsMostCommonDigitInPositionOne(input, index) ? '1' : '0'));
 		int epsilonRate = (1 << input.First().Length) - 1 - gammaRate;
 		return gammaRate * epsilonRate;
 	}
 
 	public string PartTwoFromFile(string[] inputLines) => PartTwo(inputLines).ToString();
 
-	public int PartTwo(string[] input) {
-		List<string> remainingOxygenNumbers = new(input), remainingCo2ScrubberNumbers = new(input);
-		for (int i = 0; i < input.First().Length; ++i) {
-			if (remainingOxygenNumbers.Count > 1) {
-				char charToKeep = IsMostCommonDigitInPositionOne(remainingOxygenNumbers, i) ? '1' : '0';
-				remainingOxygenNumbers.RemoveAll(s => s[i] != charToKeep);
-			}
-			if (remainingCo2ScrubberNumbers.Count > 1) {
-				char charToRemove = IsMostCommonDigitInPositionOne(remainingCo2ScrubberNumbers, i) ? '1' : '0';
-				remainingCo2ScrubberNumbers.RemoveAll(s => s[i] == charToRemove);
-			}
+	public int PartTwo(string[] input) => GetRating(input, true) * GetRating(input, false);
+
+	private static int GetRating(string[] input, bool keepMostCommon) {
+		List<string> remainingNumbers = new(input);
+		for (int i = 0; i < input.First().Length && remainingNumbers.Count > 1; ++i) {
+			bool mostCommonDigitIsOne = IsMostCommonDigitInPositionOne(remainingNumbers, i);
+			remainingNumbers.RemoveAll(s => s[i] == '1' != mostCommonDigitIsOne == keepMostCommon);
 		}
-		return Convert.ToInt32(string.Concat(remainingOxygenNumbers.Single()), 2) * Convert.ToInt32(string.Concat(remainingCo2ScrubberNumbers.Single()), 2);
+		return BinaryToInt(remainingNumbers.Single());
 	}
 
-	private bool IsMostCommonDigitInPositionOne(IEnumerable<string> values, int index) => values.Count(v => v[index] == '1') * 2 >= values.Count();
+	private static bool IsMostCommonDigitInPositionOne(IEnumerable<string> values, int index) => values.Count(v => v[index] == '1') * 2 >= values.Count();
+	private static int BinaryToInt(IEnumerable<char> s) => Convert.ToInt32(string.Concat(s), 2);
 }
