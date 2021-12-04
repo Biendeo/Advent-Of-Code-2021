25723a85dd34e83ded6d1bace65a9bdb1ead41ce
Thomas Moffet <pantherman1996@gmail.com>
2021-12-04 16:31:56 +1100

    Completed day 4. Definitely room to improve it.

---
+++ AdventOfCodeLib/Challenges/Day04.cs
@@ -0,0 +1,91 @@
+﻿namespace AdventOfCodeLib.Challenges;
+
+[DayDetails(Day = 4, Name = "Giant Squid")]
+public class Day04 : IDayChallenge {
+	private const int CheckedValue = -1;
+	private const int BoardSize = 5;
+
+	public string PartOneFromFile(string[] inputLines) {
+		(int[] drawnNumbers, List<int[,]> bingoBoards) = ParseInput(inputLines);
+		return PartOne(drawnNumbers, bingoBoards).ToString();
+	}
+
+	public int PartOne(int[] drawnNumbers, List<int[,]> bingoBoards) => bingoBoards.Select(b => GetVictoryOfBoard(drawnNumbers, b)).Aggregate((a, b) => a.turnCount < b.turnCount ? a : b).score;
+
+	public string PartTwoFromFile(string[] inputLines) {
+		(int[] drawnNumbers, List<int[,]> bingoBoards) = ParseInput(inputLines);
+		return PartTwo(drawnNumbers, bingoBoards).ToString();
+	}
+
+	public int PartTwo(int[] drawnNumbers, List<int[,]> bingoBoards) => bingoBoards.Select(b => GetVictoryOfBoard(drawnNumbers, b)).Aggregate((a, b) => a.turnCount > b.turnCount ? a : b).score;
+
+	public static (int[] drawnNumbers, List<int[,]> bingoBoards) ParseInput(string[] input) {
+		List<int[,]> bingoBoards = new();
+		for (int inputY = 1; inputY < input.Length; inputY += 6) {
+			int[,] board = new int[BoardSize, BoardSize];
+			for (int y = 0; y < BoardSize; ++y) {
+				int[] parsedRow = input[inputY + y + 1].Split(' ').Where(s => !string.IsNullOrEmpty(s)).Select(x => int.Parse(x)).ToArray();
+				for (int x = 0; x < BoardSize; ++x) {
+					board[x, y] = parsedRow[x];
+				}
+			}
+			bingoBoards.Add(board);
+		}
+		return (input[0].Split(',').Select(x => int.Parse(x)).ToArray(), bingoBoards);
+	}
+
+	private (int turnCount, int score) GetVictoryOfBoard(int[] drawnNumbers, int[,] bingoBoard) {
+		int[,] modifiedBoard = (int[,])bingoBoard.Clone();
+		int lastDrawnNumber = -1;
+		int turnCount;
+		for (turnCount = 0; turnCount < drawnNumbers.Length && !DoesBoardHaveVictory(modifiedBoard); ++turnCount) {
+			lastDrawnNumber = drawnNumbers[turnCount];
+			for (int x = 0; x < BoardSize; ++x) {
+				for (int y = 0; y < BoardSize; ++y) {
+					if (modifiedBoard[x, y] == lastDrawnNumber) {
+						modifiedBoard[x, y] = CheckedValue;
+					}
+				}
+			}
+		}
+		return (turnCount, GetBoardScore(modifiedBoard, lastDrawnNumber));
+	}
+
+	private bool DoesBoardHaveVictory(int[,] bingoBoard) {
+		// Horizontal
+		for (int y = 0; y < BoardSize; ++y) {
+			int sum = 0;
+			for (int x = 0; x < BoardSize; ++x) {
+				sum += bingoBoard[x, y];
+			}
+			if (sum == CheckedValue * BoardSize) {
+				return true;
+			}
+		}
+
+		// Vertical
+		for (int x = 0; x < BoardSize; ++x) {
+			int sum = 0;
+			for (int y = 0; y < BoardSize; ++y) {
+				sum += bingoBoard[x, y];
+			}
+			if (sum == CheckedValue * BoardSize) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+	
+	private int GetBoardScore(int[,] bingoBoard, int lastDrawnNumber) {
+		int score = 0;
+		for (int x = 0; x < BoardSize; ++x) {
+			for (int y = 0; y < BoardSize; ++y) {
+				if (bingoBoard[x, y] != CheckedValue) {
+					score += bingoBoard[x, y];
+				}
+			}
+		}
+		return score * lastDrawnNumber;
+	}
+}
---
+++ Tests/Day04Tests.cs
@@ -0,0 +1,44 @@
+﻿using AdventOfCodeLib.Challenges;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Tests;
+
+public class Day04Tests {
+	private const string TestInput = @"7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7";
+
+	[Theory]
+	[InlineData(TestInput, 4512)]
+	public void PartOneSucceeds(string input, int expectedResult) {
+		Day04 challenge = new();
+		(int[] drawnNumbers, List<int[,]> bingoBoards) = Day04.ParseInput(input.Split(Environment.NewLine));
+		Assert.Equal(expectedResult, challenge.PartOne(drawnNumbers, bingoBoards));
+	}
+
+	[Theory]
+	[InlineData(TestInput, 1924)]
+	public void PartTwoSucceeds(string input, int expectedResult) {
+		Day04 challenge = new();
+		(int[] drawnNumbers, List<int[,]> bingoBoards) = Day04.ParseInput(input.Split(Environment.NewLine));
+		Assert.Equal(expectedResult, challenge.PartTwo(drawnNumbers, bingoBoards));
+	}
+}
