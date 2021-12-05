5bf73490c182bed6e4272a1f33f98022a57a202a
Thomas Moffet <pantherman1996@gmail.com>
2021-12-05 16:48:29 +1100

    Removed boilerplate. I use an array for a little longer but it's not in the technical part. Also cheeky dictionary code.

--- AdventOfCodeLib/Challenges/Day05.cs
+++ AdventOfCodeLib/Challenges/Day05.cs
@@ -12,12 +12,10 @@ public class Day05 : IDayChallenge {
 
 	private int GetIntersections(string[] inputLines, bool countDiagonals) {
 		Dictionary<(int x, int y), int> linePositions = new();
-		foreach ((int startX, int startY, int endX, int endY) in inputLines) {
-			foreach ((int x, int y) in GetPointsWhereLineGoes(startX, startY, endX, endY, countDiagonals)) {
-				if (!linePositions.ContainsKey((x, y))) {
-					linePositions.Add((x, y), 0);
-				}
-				++linePositions[(x, y)];
+		foreach (int[] lineCoords in inputLines.Select(line => line.Replace(" -> ", ",").Split(',').Select(x => int.Parse(x)).ToArray())) {
+			foreach ((int x, int y) in GetPointsWhereLineGoes(lineCoords[0], lineCoords[1], lineCoords[2], lineCoords[3], countDiagonals)) {
+				linePositions.TryGetValue((x, y), out int oldValue);
+				linePositions[(x, y)] = oldValue + 1;
 			}
 		}
 		return linePositions.Count(keyValue => keyValue.Value > 1);
@@ -26,18 +24,8 @@ public class Day05 : IDayChallenge {
 	private List<(int x, int y)> GetPointsWhereLineGoes(int startX, int startY, int endX, int endY, bool enableDiagonals) {
 		if (startX == endX) return Enumerable.Range(Math.Min(startY, endY), Math.Abs(endY - startY) + 1).Select(y => (startX, y)).ToList();
 		if (startY == endY) return Enumerable.Range(Math.Min(startX, endX), Math.Abs(endX - startX) + 1).Select(x => (x, startY)).ToList();
-		if (enableDiagonals && startX - endX == startY - endY) return Enumerable.Range(Math.Min(startX, endX), Math.Abs(endX - startX) + 1).Select(x => (x, startY + x - startX)).ToList();
-		if (enableDiagonals && startX - endX == endY - startY) return Enumerable.Range(Math.Min(startX, endX), Math.Abs(endX - startX) + 1).Select(x => (x, startY - x + startX)).ToList();
+		if (enableDiagonals && Math.Abs(startX - endX) == Math.Abs(startY - endY))
+			return Enumerable.Range(Math.Min(startX, endX), Math.Abs(endX - startX) + 1).Select(x => (x, startY + (x - startX) * (startX - endX + startY - endY != 0 ? 1 : -1))).ToList();
 		return new();
-	}
-}
-
-internal static class Day05Extensions {
-	public static void Deconstruct(this string line, out int a0, out int a1, out int a2, out int a3) {
-		int[] values = line.Replace(" -> ", ",").Split(',').Select(x => int.Parse(x)).ToArray();
-		a0 = values[0];
-		a1 = values[1];
-		a2 = values[2];
-		a3 = values[3];
 	}
 }
