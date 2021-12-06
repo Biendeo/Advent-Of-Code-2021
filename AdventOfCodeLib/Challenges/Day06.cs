e729d3ead31eafda9f96c274d3eb3501965b7fe7
Thomas Moffet <pantherman1996@gmail.com>
2021-12-06 16:42:09 +1100

    Completed day 6. Not much more to be done.

---
+++ AdventOfCodeLib/Challenges/Day06.cs
@@ -0,0 +1,29 @@
+﻿namespace AdventOfCodeLib.Challenges;
+
+[DayDetails(Day = 6, Name = "Lanternfish")]
+public class Day06 : IDayChallenge {
+	private const int NewFishLifespan = 8;
+	private const int OldFishResetLifespan = 6;
+
+	public string PartOneFromFile(string[] inputLines) => PartOne(inputLines.Single().Split(',').Select(x => int.Parse(x)).ToList()).ToString();
+
+	public long PartOne(List<int> input) => Solve(input, 80);
+
+	public string PartTwoFromFile(string[] inputLines) => PartTwo(inputLines.Single().Split(',').Select(x => int.Parse(x)).ToList()).ToString();
+
+	public long PartTwo(List<int> input) => Solve(input, 256);
+
+	public long Solve(List<int> input, int days) {
+		Dictionary<int, long> fish = input.GroupBy(x => x).ToDictionary(x => x.Key, x => (long)x.Count());
+		foreach (int i in Enumerable.Range(0, NewFishLifespan + 1)) fish.TryAdd(i, 0L);
+		for (int day = 0; day < days; ++day) {
+			long expiringFish = fish[0];
+			for (int i = 0; i < NewFishLifespan; ++i) {
+				fish[i] = fish[i + 1];
+			}
+			fish[NewFishLifespan] = expiringFish;
+			fish[OldFishResetLifespan] += expiringFish;
+		}
+		return fish.Values.Sum();
+	}
+}
---
+++ Tests/Day06Tests.cs
@@ -0,0 +1,16 @@
+﻿using AdventOfCodeLib.Challenges;
+using System.Linq;
+using Xunit;
+
+namespace Tests;
+
+public class Day06Tests {
+	[Theory]
+	[InlineData(new int[] { 3, 4, 3, 1, 2 }, 18, 26)]
+	[InlineData(new int[] { 3, 4, 3, 1, 2 }, 80, 5934)]
+	[InlineData(new int[] { 3, 4, 3, 1, 2 }, 256, 26984457539L)]
+	public void SolveSucceeds(int[] input, int days, long expectedResult) {
+		Day06 challenge = new();
+		Assert.Equal(expectedResult, challenge.Solve(input.ToList(), days));
+	}
+}
