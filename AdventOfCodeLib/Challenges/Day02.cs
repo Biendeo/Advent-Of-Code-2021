a932fbf06928e9b56a9aa9bce39f67a9802e43f2
Thomas Moffet <pantherman1996@gmail.com>
2021-12-07 19:34:02 +1100

    Got rid of generic ArrayExtensions and just implemented day 2 straight, but with a cheeky delegate.

--- AdventOfCodeLib/Challenges/Day02.cs
+++ AdventOfCodeLib/Challenges/Day02.cs
@@ -2,45 +2,32 @@
 
 [DayDetails(Day = 2, Name = "Dive!")]
 public class Day02 : IDayChallenge {
+	private delegate (int x, int y, int aim) Operation(int mangitude, int x, int y, int aim);
+
 	public string PartOneFromFile(string[] inputLines) => PartOne(inputLines).ToString();
 
-	public int PartOne(string[] inputLines) {
-		int x = 0, y = 0;
-		foreach ((string direction, string magnitudeString) in inputLines.Select(l => l.Split(' '))) {
-			int magnitude = int.Parse(magnitudeString);
-			switch (direction) {
-				case "up":
-					y -= magnitude;
-					break;
-				case "forward":
-					x += magnitude;
-					break;
-				case "down":
-					y += magnitude;
-					break;
-			}
-		}
-		return Math.Abs(x * y);
-	}
+	public int PartOne(string[] inputLines) => FindSolution(inputLines, partOneActions);
 
 	public string PartTwoFromFile(string[] inputLines) => PartTwo(inputLines).ToString();
 
-	public int PartTwo(string[] inputLines) {
+	public int PartTwo(string[] inputLines) => FindSolution(inputLines, partTwoActions);
+
+	private static Dictionary<string, Operation> partOneActions = new() {
+		{ "up", (magnitude, x, y, aim) => (x, y - magnitude, aim) },
+		{ "forward", (magnitude, x, y, aim) => (x + magnitude, y, aim) },
+		{ "down", (magnitude, x, y, aim) => (x, y + magnitude, aim) }
+	};
+
+	private static Dictionary<string, Operation> partTwoActions = new() {
+		{ "up", (magnitude, x, y, aim) => (x, y, aim - magnitude) },
+		{ "forward", (magnitude, x, y, aim) => (x + magnitude, y + magnitude * aim, aim) },
+		{ "down", (magnitude, x, y, aim) => (x, y, aim + magnitude) }
+	};
+
+	private int FindSolution(string[] inputLines, Dictionary<string, Operation> actions) {
 		int x = 0, y = 0, aim = 0;
-		foreach ((string direction, string magnitudeString) in inputLines.Select(l => l.Split(' '))) {
-			int magnitude = int.Parse(magnitudeString);
-			switch (direction) {
-				case "up":
-					aim -= magnitude;
-					break;
-				case "forward":
-					x += magnitude;
-					y += magnitude * aim;
-					break;
-				case "down":
-					aim += magnitude;
-					break;
-			}
+		foreach ((string direction, int magnitude) in inputLines.Select(l => (l.Split(' ')[0], int.Parse(l.Split(' ')[1])))) {
+			(x, y, aim) = actions[direction](magnitude, x, y, aim);
 		}
 		return Math.Abs(x * y);
 	}
