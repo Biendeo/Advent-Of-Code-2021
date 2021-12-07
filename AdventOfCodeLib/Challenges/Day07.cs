e30d7fc818ae1fb7f55813e80c0e9c7ba3a8d1d0
Thomas Moffet <pantherman1996@gmail.com>
2021-12-07 16:25:39 +1100

    Completed day 7. Not particularly a cleaner way of doing it.

---
+++ AdventOfCodeLib/Challenges/Day07.cs
@@ -0,0 +1,16 @@
+﻿namespace AdventOfCodeLib.Challenges;
+
+[DayDetails(Day = 7, Name = "The Treachery of Whales")]
+public class Day07 : IDayChallenge {
+	private const int HopefullyLargestCrabDistance = 2000;
+
+	public string PartOneFromFile(string[] inputLines) => PartOne(inputLines.Single().Split(',').Select(x => int.Parse(x)).ToArray()).ToString();
+
+	public int PartOne(int[] input) => Enumerable.Range(0, input.Max() + 1).Select(x => input.Sum(y => Math.Abs(x - y))).Min();
+
+	public string PartTwoFromFile(string[] inputLines) => PartTwo(inputLines.Single().Split(',').Select(x => int.Parse(x)).ToArray()).ToString();
+
+	private int[] TriangleNumbers = Enumerable.Range(0, HopefullyLargestCrabDistance).Select(x => Enumerable.Range(1, x).Sum()).ToArray();
+
+	public int PartTwo(int[] input) => Enumerable.Range(0, input.Max() + 1).Select(x => input.Sum(y => TriangleNumbers[Math.Abs(x - y)])).Min();
+}
---
+++ Tests/Day07Tests.cs
@@ -0,0 +1,21 @@
+﻿using AdventOfCodeLib.Challenges;
+using System.Linq;
+using Xunit;
+
+namespace Tests;
+
+public class Day07Tests {
+	[Theory]
+	[InlineData(new int[] { 16, 1, 2, 0, 4, 2, 7, 1, 2, 14 }, 37)]
+	public void PartOneSucceeds(int[] input, int expectedResult) {
+		Day07 challenge = new();
+		Assert.Equal(expectedResult, challenge.PartOne(input));
+	}
+
+	[Theory]
+	[InlineData(new int[] { 16, 1, 2, 0, 4, 2, 7, 1, 2, 14 }, 168)]
+	public void PartTwoSucceeds(int[] input, int expectedResult) {
+		Day07 challenge = new();
+		Assert.Equal(expectedResult, challenge.PartTwo(input));
+	}
+}
