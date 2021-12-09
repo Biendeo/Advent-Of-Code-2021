dfab9e8aa6a2b439c4d999216a19a264cd492704
Thomas Moffet <pantherman1996@gmail.com>
2021-12-09 16:39:56 +1100

    Completed day 9.

---
+++ AdventOfCodeLib/Challenges/Day09.cs
@@ -0,0 +1,37 @@
+﻿namespace AdventOfCodeLib.Challenges;
+
+[DayDetails(Day = 9, Name = "Smoke Basin")]
+public class Day09 : IDayChallenge {
+	public string PartOneFromFile(string[] inputLines) => PartOne(inputLines).ToString();
+
+	public int PartOne(string[] inputLines) => GetTwoDimensionalRange(0, inputLines.First().Length - 1, 0, inputLines.Length - 1)
+		.Sum(a => GetNeighbours(a.x, a.y, 0, inputLines.First().Length - 1, 0, inputLines.Length - 1).All(b => inputLines[b.y][b.x] > inputLines[a.y][a.x]) ? inputLines[a.y][a.x] - '0' + 1 : 0);
+
+	public string PartTwoFromFile(string[] inputLines) => PartTwo(inputLines).ToString();
+
+	public int PartTwo(string[] inputLines) {
+		HashSet<(int x, int y)> seenPositions = new();
+		return GetTwoDimensionalRange(0, inputLines.First().Length - 1, 0, inputLines.Length - 1)
+			.Select(a => SizeOfBasin(a.x, a.y, inputLines, seenPositions))
+			.OrderByDescending(x => x)
+			.Take(3)
+			.Aggregate(1, (acc, x) => acc * x);
+	}
+
+	private int SizeOfBasin(int x, int y, string[] inputLines, HashSet<(int x, int y)> seenPositions) {
+		if (inputLines[y][x] == '9' || seenPositions.Contains((x, y))) return 0;
+		seenPositions.Add((x, y));
+		return GetNeighbours(x, y, 0, inputLines.First().Length - 1, 0, inputLines.Length - 1).Sum(b => SizeOfBasin(b.x, b.y, inputLines, seenPositions)) + 1;
+	}
+
+	private IEnumerable<(int x, int y)> GetTwoDimensionalRange(int startX, int endX, int startY, int endY) => Enumerable.Range(startX, endX - startX + 1).SelectMany(x => Enumerable.Range(startY, endY - startY + 1).Select(y => (x, y)));
+
+	private IEnumerable<(int x, int y)> GetNeighbours(int x, int y, int minX, int maxX, int minY, int maxY) {
+		List<(int x, int y)> r = new();
+		if (x > minX) r.Add((x - 1, y));
+		if (y > minY) r.Add((x, y - 1));
+		if (x < maxX) r.Add((x + 1, y));
+		if (y < maxY) r.Add((x, y + 1));
+		return r;
+	}
+}
---
+++ Tests/Day09Tests.cs
@@ -0,0 +1,22 @@
+﻿using AdventOfCodeLib.Challenges;
+using Xunit;
+
+namespace Tests;
+
+public class Day09Tests {
+	private const string TestInput = "2199943210,3987894921,9856789892,8767896789,9899965678";
+
+	[Theory]
+	[InlineData(TestInput, 15)]
+	public void PartOneSucceeds(string input, int expectedResult) {
+		Day09 challenge = new();
+		Assert.Equal(expectedResult, challenge.PartOne(input.Split(',')));
+	}
+
+	[Theory]
+	[InlineData(TestInput, 1134)]
+	public void PartTwoSucceeds(string input, int expectedResult) {
+		Day09 challenge = new();
+		Assert.Equal(expectedResult, challenge.PartTwo(input.Split(',')));
+	}
+}
