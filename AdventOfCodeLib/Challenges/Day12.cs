3ff6fba1541e55be9b449cd67fbe8721a364ad3a
Thomas Moffet <pantherman1996@gmail.com>
2021-12-12 16:34:32 +1100

    Completed day 12.

---
+++ AdventOfCodeLib/Challenges/Day12.cs
@@ -0,0 +1,50 @@
+﻿namespace AdventOfCodeLib.Challenges;
+
+[DayDetails(Day = 12, Name = "Passage Pathing")]
+public class Day12 : IDayChallenge {
+	public string PartOneFromFile(string[] inputLines) => PartOne(inputLines).ToString();
+
+	public int PartOne(string[] inputLines) {
+		(HashSet<string> caves, HashSet<(string, string)> connections) = ParseInput(inputLines);
+		return CountCavePaths("start", new(), caves, connections, false, false);
+	}
+
+	public string PartTwoFromFile(string[] inputLines) => PartTwo(inputLines).ToString();
+
+	public int PartTwo(string[] inputLines) {
+		(HashSet<string> caves, HashSet<(string, string)> connections) = ParseInput(inputLines);
+		return CountCavePaths("start", new(), caves, connections, true, false);
+	}
+
+	private (HashSet<string>, HashSet<(string, string)>) ParseInput(string[] inputLines) {
+		HashSet<string> caves = new();
+		HashSet<(string, string)> connections = new();
+		foreach ((string cave1, string cave2) in inputLines.Select(s => (s.Split('-')[0], s.Split('-')[1]))) {
+			caves.Add(cave1);
+			caves.Add(cave2);
+			connections.Add((cave1, cave2));
+			connections.Add((cave2, cave1));
+		}
+		return (caves, connections);
+	}
+
+	private static bool IsAllLowercase(string s) => s.All(c => c >= 'a' && c <= 'z');
+
+	private int CountCavePaths(string currentCave, Stack<string> pathSoFar, HashSet<string> caves, HashSet<(string, string)> connections, bool partTwoRules, bool hasVisitedSmallCaveTwice) {
+		if (partTwoRules && pathSoFar.Any() && currentCave == "start") {
+			return 0;
+		} else if (IsAllLowercase(currentCave) && pathSoFar.Contains(currentCave)) {
+			if (partTwoRules && !hasVisitedSmallCaveTwice) {
+				hasVisitedSmallCaveTwice = true;
+			} else {
+				return 0;
+			}
+		} else if (currentCave == "end") {
+			return 1;
+		}
+		pathSoFar.Push(currentCave);
+		int pathsToEnd = connections.Where(c => c.Item1 == currentCave).Sum(c => CountCavePaths(c.Item2, pathSoFar, caves, connections, partTwoRules, hasVisitedSmallCaveTwice));
+		pathSoFar.Pop();
+		return pathsToEnd;
+	}
+}
---
+++ Tests/Day12Tests.cs
@@ -0,0 +1,61 @@
+﻿using AdventOfCodeLib.Challenges;
+using System;
+using Xunit;
+
+namespace Tests;
+
+public class Day12Tests {
+	public const string TestInputShort = @"start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end";
+	public const string TestInputLong = @"dc-end
+HN-start
+start-kj
+dc-start
+dc-HN
+LN-dc
+HN-end
+kj-sa
+kj-HN
+kj-dc";
+	public const string TestInputReallyLong = @"fs-end
+he-DX
+fs-he
+start-DX
+pj-DX
+end-zg
+zg-sl
+zg-pj
+pj-he
+RW-he
+fs-DX
+pj-RW
+zg-RW
+start-pj
+he-WI
+zg-he
+pj-fs
+start-RW";
+
+	[Theory]
+	[InlineData(TestInputShort, 10)]
+	[InlineData(TestInputLong, 19)]
+	[InlineData(TestInputReallyLong, 226)]
+	public void PartOneSucceeds(string input, int expectedResult) {
+		Day12 challenge = new();
+		Assert.Equal(expectedResult, challenge.PartOne(input.Split(Environment.NewLine)));
+	}
+
+	[Theory]
+	[InlineData(TestInputShort, 36)]
+	[InlineData(TestInputLong, 103)]
+	[InlineData(TestInputReallyLong, 3509)]
+	public void PartTwoSucceeds(string input, int expectedResult) {
+		Day12 challenge = new();
+		Assert.Equal(expectedResult, challenge.PartTwo(input.Split(Environment.NewLine)));
+	}
+}
