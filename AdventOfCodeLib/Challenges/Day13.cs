074f56055436c507b813d1c9c6ebfbd5226ca99f
Thomas Moffet <pantherman1996@gmail.com>
2021-12-13 16:31:49 +1100

    Completed day 13.

---
+++ AdventOfCodeLib/Challenges/Day13.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+
+namespace AdventOfCodeLib.Challenges;
+
+[DayDetails(Day = 13, Name = "Transparent Origami")]
+public class Day13 : IDayChallenge {
+	public string PartOneFromFile(string[] inputLines) => PartOne(inputLines).ToString();
+
+	public int PartOne(string[] inputLines) {
+		(HashSet<(int x, int y)> dots, List<(bool xFold, int value)> folds) = ParseInput(inputLines);
+		return Fold(dots, folds.First().xFold, folds.First().value).Count;
+	}
+
+	private (HashSet<(int x, int y)> dots, List<(bool xFold, int value)> folds) ParseInput(string[] inputLines) {
+		HashSet<(int x, int y)> dots = new();
+		List<(bool xFold, int value)> folds = new();
+		bool blankLine = false;
+		foreach (string line in inputLines) {
+			if (line == string.Empty) blankLine = true;
+			else if (!blankLine) dots.Add((int.Parse(line.Split(',')[0]), int.Parse(line.Split(',')[1])));
+			else folds.Add((line[11] == 'x', int.Parse(line[13..])));
+		}
+		return (dots, folds);
+	}
+
+	private HashSet<(int x, int y)> Fold(HashSet<(int x, int y)> dots, bool xFold, int value) {
+		HashSet<(int x, int y)> newDots = new();
+		foreach ((int x, int y) in dots) {
+			if (xFold) newDots.Add((x > value ? 2 * value - x : x, y));
+			else newDots.Add((x, y > value ? 2 * value - y : y));
+		}
+		return newDots;
+	}
+
+	public string PartTwoFromFile(string[] inputLines) => PartTwo(inputLines);
+
+	public string PartTwo(string[] inputLines) {
+		(HashSet<(int x, int y)> dots, List<(bool xFold, int value)> folds) = ParseInput(inputLines);
+		foreach ((bool xFold, int value) in folds) {
+			dots = Fold(dots, xFold, value);
+		}
+		StringBuilder sb = new();
+		int maxX = dots.Max(d => d.x);
+		int maxY = dots.Max(d => d.y);
+		for (int y = 0; y <= maxY; ++y) {
+			for (int x = 0; x <= maxX; ++x) {
+				sb.Append(dots.Contains((x, y)) ? '#' : ' ');
+			}
+			if (y != maxY) sb.AppendLine();
+		}
+		return sb.ToString();
+	}
+}
---
+++ Tests/Day13Tests.cs
@@ -0,0 +1,49 @@
+﻿using AdventOfCodeLib.Challenges;
+using System;
+using Xunit;
+
+namespace Tests;
+
+public class Day13Tests {
+	public const string TestInput = @"6,10
+0,14
+9,10
+0,3
+10,4
+4,11
+6,0
+6,12
+4,1
+0,13
+10,12
+3,4
+3,0
+8,4
+1,10
+2,14
+8,10
+9,0
+
+fold along y=7
+fold along x=5";
+
+	[Theory]
+	[InlineData(TestInput, 17)]
+	public void PartOneSucceeds(string input, int expectedResult) {
+		Day13 challenge = new();
+		Assert.Equal(expectedResult, challenge.PartOne(input.Split(Environment.NewLine)));
+	}
+
+	public const string ExpectedPartTwo = @"#####
+#   #
+#   #
+#   #
+#####";
+
+	[Theory]
+	[InlineData(TestInput, ExpectedPartTwo)]
+	public void PartTwoSucceeds(string input, string expectedResult) {
+		Day13 challenge = new();
+		Assert.Equal(expectedResult, challenge.PartTwo(input.Split(Environment.NewLine)));
+	}
+}
