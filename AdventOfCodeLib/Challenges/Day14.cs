b50672fa87cf91cf470debaae369ba32696f6470
Thomas Moffet <pantherman1996@gmail.com>
2021-12-14 16:46:37 +1100

    Completed day 14.

---
+++ AdventOfCodeLib/Challenges/Day14.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace AdventOfCodeLib.Challenges;
+
+[DayDetails(Day = 14, Name = "Extended Polymerization")]
+public class Day14 : IDayChallenge {
+	public string PartOneFromFile(string[] inputLines) => PartOne(inputLines).ToString();
+
+	public long PartOne(string[] inputLines) => ComputeGreatestDifferenceInFrequencies(inputLines, 10);
+
+	public string PartTwoFromFile(string[] inputLines) => PartTwo(inputLines).ToString();
+
+	public long PartTwo(string[] inputLines) => ComputeGreatestDifferenceInFrequencies(inputLines, 40);
+
+	private (string template, Dictionary<(char, char), char> insertionRules) ParseInput(string[] inputLines) => (inputLines.First(), inputLines[2..].ToDictionary(l => (l[0], l[1]), l => l[6]));
+
+	private long ComputeGreatestDifferenceInFrequencies(string[] inputLines, int generations) {
+		(string template, Dictionary<(char, char), char> insertionRules) = ParseInput(inputLines);
+		char lastCharacter = template.Last();
+		Dictionary<(char, char), long> pairingFrequencies = new();
+		for (int i = 0; i < template.Length - 1; ++i) InsertAndIncrement(pairingFrequencies, (template[i], template[i + 1]), 0L, 1L);
+		for (int gen = 0; gen < generations; ++gen) pairingFrequencies = ComputeGeneration(pairingFrequencies, insertionRules);
+		Dictionary<char, long> elementFrequencies = new();
+		foreach (KeyValuePair<(char, char), long> frequency in pairingFrequencies) InsertAndIncrement(elementFrequencies, frequency.Key.Item1, 0L, frequency.Value);
+		++elementFrequencies[lastCharacter];
+		return elementFrequencies.Max(e => e.Value) - elementFrequencies.Min(e => e.Value);
+	}
+
+	private Dictionary<(char, char), long> ComputeGeneration(Dictionary<(char, char), long> pairingFrequencies, Dictionary<(char, char), char> insertionRules) {
+		Dictionary<(char, char), long> newPairingFrequencies = new();
+		foreach (KeyValuePair<(char, char), char> insertionRule in insertionRules) {
+			if (pairingFrequencies.TryGetValue(insertionRule.Key, out long x)) {
+				InsertAndIncrement(newPairingFrequencies, (insertionRule.Key.Item1, insertionRule.Value), 0L, x);
+				InsertAndIncrement(newPairingFrequencies, (insertionRule.Value, insertionRule.Key.Item2), 0L, x);
+			}
+		}
+		return newPairingFrequencies;
+	}
+
+	private static void InsertAndIncrement<TKey>(Dictionary<TKey, long> d, TKey key, long startingValue, long incrementValue) where TKey : notnull {
+		d.TryAdd(key, startingValue);
+		d[key] += incrementValue;
+	}
+}
---
+++ Tests/Day14Tests.cs
@@ -0,0 +1,40 @@
+﻿using AdventOfCodeLib.Challenges;
+using System;
+using Xunit;
+
+namespace Tests;
+
+public class Day14Tests {
+	private const string TestInput = @"NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C";
+
+	[Theory]
+	[InlineData(TestInput, 1588L)]
+	public void PartOneSucceeds(string input, long expectedResult) {
+		Day14 challenge = new();
+		Assert.Equal(expectedResult, challenge.PartOne(input.Split(Environment.NewLine)));
+	}
+
+	[Theory]
+	[InlineData(TestInput, 2188189693529L)]
+	public void PartTwoSucceeds(string input, long expectedResult) {
+		Day14 challenge = new();
+		Assert.Equal(expectedResult, challenge.PartTwo(input.Split(Environment.NewLine)));
+	}
+}
