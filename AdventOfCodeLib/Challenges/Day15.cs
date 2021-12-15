2613552652fa31b63bcc16e037fb5cc11aca6557
Thomas Moffet <pantherman1996@gmail.com>
2021-12-15 16:58:34 +1100

    Completed day 15.

---
+++ AdventOfCodeLib/Challenges/Day15.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+
+namespace AdventOfCodeLib.Challenges;
+
+[DayDetails(Day = 15, Name = "Chiton")]
+public class Day15 : IDayChallenge {
+	public string PartOneFromFile(string[] inputLines) => PartOne(inputLines).ToString();
+
+	public int PartOne(string[] inputLines) {
+		int[,] costs = new int[inputLines.First().Length, inputLines.Length];
+
+		PriorityQueue<(int x, int y), int> searchQueue = new();
+		searchQueue.Enqueue((costs.GetLength(0) - 1, costs.GetLength(1) - 1), inputLines.Last().Last() - '0');
+		costs[costs.GetLength(0) - 1, costs.GetLength(1) - 1] = inputLines.Last().Last() - '0';
+
+		while (searchQueue.Count > 0) {
+			(int currentX, int currentY) = searchQueue.Dequeue();
+			foreach ((int newX, int newY) in Helper.GetNeighbors(currentX, currentY, 0, costs.GetLength(0) - 1, 0, costs.GetLength(1) - 1, false)) {
+				if (costs[newX, newY] == 0) {
+					costs[newX, newY] = costs[currentX, currentY] + inputLines[newY][newX] - '0';
+					searchQueue.Enqueue((newX, newY), costs[newX, newY]);
+				}
+			}
+		}
+
+		return costs[0, 0] - inputLines[0][0] + '0';
+	}
+
+	public string PartTwoFromFile(string[] inputLines) => PartTwo(inputLines).ToString();
+
+	public int PartTwo(string[] inputLines) {
+		string[] newInput = new string[inputLines.Length * 5];
+		for (int bigY = 0; bigY < 5; ++bigY) {
+			for (int y = 0; y < inputLines.Length; ++y) {
+				StringBuilder sb = new();
+				for (int bigX = 0; bigX < 5; ++bigX) {
+					sb.Append(string.Concat(inputLines[y].Select(c => (char)(((c - '1' + bigX + bigY) % 9) + '1'))));
+				}
+				newInput[bigY * inputLines.Length + y] = sb.ToString();
+			}
+		}
+		return PartOne(newInput);
+	}
+}
---
+++ Tests/Day15Tests.cs
@@ -0,0 +1,32 @@
+﻿using AdventOfCodeLib.Challenges;
+using System;
+using Xunit;
+
+namespace Tests;
+
+public class Day15Tests {
+	private const string TestInput = @"1163751742
+1381373672
+2136511328
+3694931569
+7463417111
+1319128137
+1359912421
+3125421639
+1293138521
+2311944581";
+
+	[Theory]
+	[InlineData(TestInput, 40)]
+	public void PartOneSucceeds(string input, int expectedResult) {
+		Day15 challenge = new();
+		Assert.Equal(expectedResult, challenge.PartOne(input.Split(Environment.NewLine)));
+	}
+
+	[Theory]
+	[InlineData(TestInput, 315)]
+	public void PartTwoSucceeds(string input, int expectedResult) {
+		Day15 challenge = new();
+		Assert.Equal(expectedResult, challenge.PartTwo(input.Split(Environment.NewLine)));
+	}
+}
