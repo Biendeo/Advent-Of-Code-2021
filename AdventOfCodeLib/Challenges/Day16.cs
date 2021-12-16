6fef3fcff136ae4ce2a066dc0b36994fa67b58a2
Thomas Moffet <pantherman1996@gmail.com>
2021-12-16 18:02:26 +1100

    Completed day 16.

---
+++ AdventOfCodeLib/Challenges/Day16.cs
@@ -0,0 +1,90 @@
+﻿namespace AdventOfCodeLib.Challenges;
+
+[DayDetails(Day = 16, Name = "Packet Decoder")]
+public class Day16 : IDayChallenge {
+	private delegate (int x, int y, int aim) Operation(int mangitude, int x, int y, int aim);
+
+	public string PartOneFromFile(string[] inputLines) => PartOne(inputLines.Single()).ToString();
+
+	public int PartOne(string input) {
+		(int versionSum, _) = GetPacketVersionSumAndValue(ToBinary(input), out _);
+		return versionSum;
+	}
+
+	public string PartTwoFromFile(string[] inputLines) => PartTwo(inputLines.Single()).ToString();
+
+	public long PartTwo(string input) {
+		(_, long valueSum) = GetPacketVersionSumAndValue(ToBinary(input), out _);
+		return valueSum;
+	}
+
+	private static string ToBinary(string input) => string.Concat(input.Select(c => HexToBinary[c]));
+
+	private (int versionSum, long value) GetPacketVersionSumAndValue(string binaryInput, out int packetLength) {
+		int versionSum = Convert.ToInt32(binaryInput[0..3], 2);
+		int packetTypeId = Convert.ToInt32(binaryInput[3..6], 2);
+		long value = 0;
+		if (packetTypeId == 4) {
+			int lastValue = 16;
+			packetLength = 6;
+			while (lastValue >= 16) {
+				lastValue = Convert.ToInt32(binaryInput[packetLength..(packetLength + 5)], 2);
+				packetLength += 5;
+				value = 16L * value + lastValue % 16;
+			}
+		} else {
+			List<long> values = new();
+			if (binaryInput[6] == '0') {
+				int lengthOfBits = Convert.ToInt32(binaryInput[7..22], 2);
+				packetLength = 22;
+				int packetNum = 0;
+				while (packetLength != 22 + lengthOfBits) {
+					(int subVersion, long subValue) = GetPacketVersionSumAndValue(binaryInput[packetLength..], out int subPacketLength);
+					packetLength += subPacketLength;
+					versionSum += subVersion;
+					values.Add(subValue);
+					++packetNum;
+				}
+			} else {
+				int numberOfSubPackets = Convert.ToInt32(binaryInput[7..18], 2);
+				packetLength = 18;
+				for (int i = 0; i < numberOfSubPackets; ++i) {
+					(int subVersion, long subValue) = GetPacketVersionSumAndValue(binaryInput[packetLength..], out int subPacketLength);
+					versionSum += subVersion;
+					values.Add(subValue);
+					packetLength += subPacketLength;
+				}
+			}
+			value = packetTypeId switch {
+				0 => values.Sum(),
+				1 => values.Aggregate((a, b) => a * b),
+				2 => values.Min(),
+				3 => values.Max(),
+				5 => values[0] > values[1] ? 1 : 0,
+				6 => values[0] < values[1] ? 1 : 0,
+				7 => values[0] == values[1] ? 1 : 0,
+				_ => throw new Exception($"Weird packet type ID {packetTypeId}")
+			};
+		}
+		return (versionSum, value);
+	}
+
+	private static readonly Dictionary<char, string> HexToBinary = new() {
+		{ '0', "0000" },
+		{ '1', "0001" },
+		{ '2', "0010" },
+		{ '3', "0011" },
+		{ '4', "0100" },
+		{ '5', "0101" },
+		{ '6', "0110" },
+		{ '7', "0111" },
+		{ '8', "1000" },
+		{ '9', "1001" },
+		{ 'A', "1010" },
+		{ 'B', "1011" },
+		{ 'C', "1100" },
+		{ 'D', "1101" },
+		{ 'E', "1110" },
+		{ 'F', "1111" }
+	};
+}
---
+++ Tests/Day16Tests.cs
@@ -0,0 +1,31 @@
+﻿using AdventOfCodeLib.Challenges;
+using Xunit;
+
+namespace Tests;
+
+public class Day16Tests {
+	[Theory]
+	[InlineData("D2FE28", 6)]
+	[InlineData("8A004A801A8002F478", 16)]
+	[InlineData("620080001611562C8802118E34", 12)]
+	[InlineData("C0015000016115A2E0802F182340", 23)]
+	[InlineData("A0016C880162017C3686B18A3D4780", 31)]
+	public void PartOneSucceeds(string input, int expectedResult) {
+		Day16 challenge = new();
+		Assert.Equal(expectedResult, challenge.PartOne(input));
+	}
+
+	[Theory]
+	[InlineData("C200B40A82", 3L)]
+	[InlineData("04005AC33890", 54L)]
+	[InlineData("880086C3E88112", 7L)]
+	[InlineData("CE00C43D881120", 9L)]
+	[InlineData("D8005AC2A8F0", 1L)]
+	[InlineData("F600BC2D8F", 0L)]
+	[InlineData("9C005AC2F8F0", 0L)]
+	[InlineData("9C0141080250320F1802104A08", 1L)]
+	public void PartTwoSucceeds(string input, long expectedResult) {
+		Day16 challenge = new();
+		Assert.Equal(expectedResult, challenge.PartTwo(input));
+	}
+}
