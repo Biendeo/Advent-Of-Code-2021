54cd5dd50fb7cb186493c16ba14c01a73f5dcf9e
Thomas Moffet <pantherman1996@gmail.com>
2021-12-17 16:32:32 +1100

    Completed day 17.

---
+++ AdventOfCodeLib/Challenges/Day17.cs
@@ -0,0 +1,30 @@
+﻿namespace AdventOfCodeLib.Challenges;
+
+[DayDetails(Day = 17, Name = "Trick Shot")]
+public class Day17 : IDayChallenge {
+	public string PartOneFromFile(string[] inputLines) => PartOne(inputLines.Single()).ToString();
+
+	public int PartOne(string input) => ComputeAndEvaluate(input, q => q.Max(r => r.highestY));
+
+	public string PartTwoFromFile(string[] inputLines) => PartTwo(inputLines.Single()).ToString();
+
+	public int PartTwo(string input) => ComputeAndEvaluate(input, q => q.Count());
+
+	private int ComputeAndEvaluate(string input, Func<ParallelQuery<(bool lands, int highestY)>, int> f) {
+		int[] inputValues = input.Replace(", y=", "..")[15..].Split("..").Select(a => int.Parse(a)).ToArray();
+		return f(Helper.GetTwoDimensionalRange(0, 1000, -1000, 1000).AsParallel().Select(a => DoesLandInArea(a.x, a.y, inputValues[0], inputValues[1], inputValues[3], inputValues[2])).Where(r => r.lands));
+	}
+
+	private (bool lands, int highestY) DoesLandInArea(int xVel, int yVel, int xLeft, int xRight, int yTop, int yBottom) {
+		int xv = xVel, yv = yVel, x = 0, y = 0, highestY = 0;
+		while (y >= yBottom) {
+			x += xv;
+			y += yv;
+			highestY = Math.Max(y, highestY);
+			if (x >= xLeft && x <= xRight && y >= yBottom && y <= yTop) return (true, highestY);
+			xv = xv > 0 ? xv - 1 : (xv < 0 ? xv + 1 : 0);
+			yv -= 1;
+		}
+		return (false, highestY);
+	}
+}
---
+++ Tests/Day17Tests.cs
@@ -0,0 +1,22 @@
+﻿using AdventOfCodeLib.Challenges;
+using Xunit;
+
+namespace Tests;
+
+public class Day17Tests {
+	private const string TestInput = "target area: x=20..30, y=-10..-5";
+
+	[Theory]
+	[InlineData(TestInput, 45)]
+	public void PartOneSucceeds(string input, int expectedResult) {
+		Day17 challenge = new();
+		Assert.Equal(expectedResult, challenge.PartOne(input));
+	}
+
+	[Theory]
+	[InlineData(TestInput, 112)]
+	public void PartTwoSucceeds(string input, int expectedResult) {
+		Day17 challenge = new();
+		Assert.Equal(expectedResult, challenge.PartTwo(input));
+	}
+}
