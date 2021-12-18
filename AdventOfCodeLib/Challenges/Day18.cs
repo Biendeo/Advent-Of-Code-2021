d2e7c37c066795cb6ead0150d7da39a629057ed9
Thomas Moffet <pantherman1996@gmail.com>
2021-12-18 20:50:52 +1100

    Completed day 18.

---
+++ AdventOfCodeLib/Challenges/Day18.cs
@@ -0,0 +1,79 @@
+﻿namespace AdventOfCodeLib.Challenges;
+
+[DayDetails(Day = 18, Name = "Snailfish")]
+public class Day18 : IDayChallenge {
+	public string PartOneFromFile(string[] inputLines) => PartOne(inputLines).ToString();
+
+	public int PartOne(string[] inputLines) => Magnitude(inputLines.Aggregate((a, s) => Reduce($"[{a},{s}]")));
+
+	public string PartTwoFromFile(string[] inputLines) => PartTwo(inputLines).ToString();
+
+	public int PartTwo(string[] inputLines) => Helper.GetTwoDimensionalRange(0, inputLines.Length - 1, 0, inputLines.Length - 1).Max(a => Magnitude(Reduce($"[{inputLines[a.x]},{inputLines[a.y]}]")));
+
+	public string Reduce(string line) {
+		bool didSomethingThisPass;
+		do {
+			didSomethingThisPass = false;
+			int depth = 0;
+			for (int i = 0; i < line.Length; ++i) {
+				if (line[i] == '[') {
+					++depth;
+				} else if (line[i] == ']') {
+					if (depth > 4) {
+						int indexOfThisLeftBracket = line[0..i].LastIndexOf('[');
+						int indexOfThisComma = line[0..i].LastIndexOf(',');
+						int indexOfNextLeftDigit = line[0..indexOfThisLeftBracket].LastIndexOfAny(new[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' });
+						int indexOfNextRightDigit = line[i..].IndexOfAny(new[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' }) + i;
+						if (indexOfNextLeftDigit == -1) {
+							int indexOfEndOfRightDigit = line[indexOfNextRightDigit..].IndexOfAny(new[] { ']', ',' }) + indexOfNextRightDigit;
+							line = line[..indexOfThisLeftBracket] + "0" + line[(i + 1)..indexOfNextRightDigit] + $"{int.Parse(line[indexOfNextRightDigit..indexOfEndOfRightDigit]) + int.Parse(line[(indexOfThisComma + 1)..i])}" + line[indexOfEndOfRightDigit..];
+						} else if (indexOfNextRightDigit == i - 1) {
+							int indexOfBeginningOfLeftDigit = line[..indexOfNextLeftDigit].LastIndexOfAny(new[] { '[', ',' });
+							line = line[..(indexOfBeginningOfLeftDigit + 1)] + $"{int.Parse(line[(indexOfBeginningOfLeftDigit + 1)..(indexOfNextLeftDigit + 1)]) + int.Parse(line[(indexOfThisLeftBracket + 1)..indexOfThisComma])}" + line[(indexOfNextLeftDigit + 1)..indexOfThisLeftBracket] + "0" + line[(i + 1)..];
+						} else {
+							int indexOfBeginningOfLeftDigit = line[..indexOfNextLeftDigit].LastIndexOfAny(new[] { '[', ',' });
+							int indexOfEndOfRightDigit = line[indexOfNextRightDigit..].IndexOfAny(new[] { ']', ',' }) + indexOfNextRightDigit;
+							line = line[..(indexOfBeginningOfLeftDigit + 1)] + $"{int.Parse(line[(indexOfBeginningOfLeftDigit + 1)..(indexOfNextLeftDigit + 1)]) + int.Parse(line[(indexOfThisLeftBracket + 1)..indexOfThisComma])}" + line[(indexOfNextLeftDigit + 1)..indexOfThisLeftBracket] + "0" + line[(i + 1)..indexOfNextRightDigit] + $"{int.Parse(line[indexOfNextRightDigit..indexOfEndOfRightDigit]) + int.Parse(line[(indexOfThisComma + 1)..i])}" + line[indexOfEndOfRightDigit..];
+						}
+						didSomethingThisPass = true;
+						break;
+					}
+					--depth;
+				}
+			}
+			bool lastCharacterWasDigit = false;
+			for (int i = 0; i < line.Length && !didSomethingThisPass; ++i) {
+				if (line[i] >= '0' && line[i] <= '9') {
+					if (lastCharacterWasDigit) {
+						int numberToSplit = int.Parse(line[(i - 1)..(i + 1)]);
+						line = line[..(i - 1)] + $"[{numberToSplit / 2},{(numberToSplit + 1) / 2}]" + line[(i + 1)..];
+						didSomethingThisPass = true;
+						break;
+					}
+					lastCharacterWasDigit = true;
+				} else {
+					lastCharacterWasDigit = false;
+				}
+			}
+		} while (didSomethingThisPass);
+		return line;
+	}
+
+	private int Magnitude(string line) {
+		if (line.Length == 5) {
+			return 3 * (line[1] - '0') + 2 * (line[3] - '0');
+		}
+		int indexOfComma = -1;
+		int depth = 0;
+		for (int i = 0; i < line.Length; ++i) {
+			if (line[i] == '[') {
+				++depth;
+			} else if (line[i] == ']') {
+				--depth;
+			} else if (line[i] == ',' && depth == 1) {
+				indexOfComma = i;
+			}
+		}
+		return 3 * (indexOfComma == 2 ? (line[1] - '0') : Magnitude(line[1..indexOfComma])) + 2 * (indexOfComma == line.Length - 3 ? (line[^2] - '0') : Magnitude(line[(indexOfComma + 1)..^1]));
+	}
+}
---
+++ Tests/Day18Tests.cs
@@ -0,0 +1,49 @@
+﻿using AdventOfCodeLib.Challenges;
+using System;
+using Xunit;
+
+namespace Tests;
+
+public class Day18Tests {
+	private const string TestInput = @"[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
+[[[5,[2,8]],4],[5,[[9,9],0]]]
+[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
+[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
+[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
+[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
+[[[[5,4],[7,7]],8],[[8,3],8]]
+[[9,3],[[9,9],[6,[4,9]]]]
+[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
+[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]";
+
+	[Theory]
+	[InlineData("[[1,2],[[3,4],5]]", 143)]
+	[InlineData("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]", 1384)]
+	[InlineData("[[[[1,1],[2,2]],[3,3]],[4,4]]", 445)]
+	[InlineData("[[[[3,0],[5,3]],[4,4]],[5,5]]", 791)]
+	[InlineData("[[[[5,0],[7,4]],[5,5]],[6,6]]", 1137)]
+	[InlineData("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]", 3488)]
+	[InlineData(TestInput, 4140)]
+	public void PartOneSucceeds(string input, int expectedResult) {
+		Day18 challenge = new();
+		Assert.Equal(expectedResult, challenge.PartOne(input.Split(Environment.NewLine)));
+	}
+
+	[Theory]
+	[InlineData("[[[[[9,8],1],2],3],4]", "[[[[0,9],2],3],4]")]
+	[InlineData("[7,[6,[5,[4,[3,2]]]]]", "[7,[6,[5,[7,0]]]]")]
+	[InlineData("[[6,[5,[4,[3,2]]]],1]", "[[6,[5,[7,0]]],3]")]
+	[InlineData("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]", "[[3,[2,[8,0]]],[9,[5,[7,0]]]]")]
+	[InlineData("[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]", "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]")]
+	public void ReduceSucceeds(string input, string expectedResult) {
+		Day18 challenge = new();
+		Assert.Equal(expectedResult, challenge.Reduce(input));
+	}
+
+	[Theory]
+	[InlineData(TestInput, 3993)]
+	public void PartTwoSucceeds(string input, int expectedResult) {
+		Day18 challenge = new();
+		Assert.Equal(expectedResult, challenge.PartTwo(input.Split(Environment.NewLine)));
+	}
+}
