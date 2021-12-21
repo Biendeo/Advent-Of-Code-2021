25da3a6d30bb3efc59996043dc087ab9d3cda7bc
Thomas Moffet <pantherman1996@gmail.com>
2021-12-21 21:01:59 +1100

    Completed day 20.

---
+++ AdventOfCodeLib/Challenges/Day20.cs
@@ -0,0 +1,46 @@
+﻿namespace AdventOfCodeLib.Challenges;
+
+[DayDetails(Day = 20, Name = "Trench Map")]
+public class Day20 : IDayChallenge {
+	public string PartOneFromFile(string[] inputLines) => PartOne(inputLines).ToString();
+
+	public int PartOne(string[] inputLines) => EnhancedPixelCount(inputLines, 2);
+
+	public string PartTwoFromFile(string[] inputLines) => PartTwo(inputLines).ToString();
+
+	public int PartTwo(string[] inputLines) => EnhancedPixelCount(inputLines, 50);
+
+	private int EnhancedPixelCount(string[] inputLines, int passes) {
+		(bool[] enhancement, HashSet<(int x, int y)> image) = ParseInput(inputLines);
+		for (int i = 0; i < passes; ++i) {
+			image = Enhance(enhancement, image, passes);
+		}
+		return image.Where(a => a.x >= (-1 * passes) && a.y >= (-1 * passes) && a.x < (inputLines[2].Length + 1 * passes) && a.y < (inputLines.Length - 2 + 1 * passes)).Count();
+	}
+
+	private HashSet<(int x, int y)> Enhance(bool[] enhancement, HashSet<(int x, int y)> image, int passes) {
+		HashSet<(int x, int y)> newImage = new();
+		Helper.GetTwoDimensionalRange(-200, 200, -200, 200).AsParallel().ForAll(a => {
+			int foundNum = 0;
+			foreach ((int y, int x) in Helper.GetTwoDimensionalRange(a.y - 1, a.y + 1, a.x - 1, a.x + 1)) {
+				foundNum = foundNum * 2 + (image.Contains((x, y)) ? 1 : 0);
+			}
+			if (enhancement[foundNum]) {
+				lock (newImage) {
+					newImage.Add((a.x, a.y));
+				}
+			}
+		});
+		return newImage;
+	}
+
+	private (bool[] enhancement, HashSet<(int x, int y)> image) ParseInput(string[] inputLines) {
+		HashSet<(int x, int y)> image = new();
+		foreach ((int x, int y) in Helper.GetTwoDimensionalRange(0, inputLines[2].Length - 1, 0, inputLines.Length - 3)) {
+			if (inputLines[y + 2][x] == '#') {
+				image.Add((x, y));
+			}
+		}
+		return (inputLines[0].Select(c => c == '#').ToArray(), image);
+	}
+}
---
+++ Tests/Day20Tests.cs
@@ -0,0 +1,29 @@
+﻿using AdventOfCodeLib.Challenges;
+using System;
+using Xunit;
+
+namespace Tests;
+
+public class Day20Tests {
+	private const string TestInput = @"..#.#..#####.#.#.#.###.##.....###.##.#..###.####..#####..#....#..#..##..###..######.###...####..#..#####..##..#.#####...##.#.#..#.##..#.#......#.###.######.###.####...#.##.##..#..#..#####.....#.#....###..#.##......#.....#..#..#..##..#...##.######.####.####.#.#...#.......#..#.#.#...####.##.#......#..#...##.#.##..#...##.#.##..###.#......#.#.......#.#.#.####.###.##...#.....####.#..#..#.##.#....##..#.####....##...##..#...#......#.#.......#.......##..####..#...#.#.#...##..#.#..###..#####........#..####......#..#
+
+#..#.
+#....
+##..#
+..#..
+..###";
+
+	[Theory]
+	[InlineData(TestInput, 35)]
+	public void PartOneSucceeds(string input, int expectedResult) {
+		Day20 challenge = new();
+		Assert.Equal(expectedResult, challenge.PartOne(input.Split(Environment.NewLine)));
+	}
+
+	[Theory]
+	[InlineData(TestInput, 3351)]
+	public void PartTwoSucceeds(string input, int expectedResult) {
+		Day20 challenge = new();
+		Assert.Equal(expectedResult, challenge.PartTwo(input.Split(Environment.NewLine)));
+	}
+}
